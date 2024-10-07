3be3411775414fcb7864cff2861ea81af3515256
Louis Hislaire <122865223+GitAzuXo@users.noreply.github.com>
2024-10-07 11:05:24 +0200

    Home implementation

--- webapp_clientside/src/router/index.js
+++ webapp_clientside/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
@@ -8,18 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/cars',
-      redirect: '/cars/list/all',
-    },
-    {
-      path: '/cars/:action/:id',
-      name: 'cars',
-      component: HelloWorld,
-      props: true
+      name: 'Home',
+      component: Home
     }
   ]
 })
